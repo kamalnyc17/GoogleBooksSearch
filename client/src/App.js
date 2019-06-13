0eb7ceaaccea72fe3d6f71bdccfb5c3daebaeba7
Kamal Mukherjee <kamalnyc17@gmail.com>
2019-06-13 07:42:54 -0400

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Save from "./pages/Saved";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/saved" component={Save} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ routes/api/search.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const booksController = require("../../controllers/booksController");
+
+// Matches with "/api/search"
+router.route("/")
+  .get(booksController.findAll)
+  .post(booksController.create);
+
+// Matches with "/api/search/:id"
+router
+  .route("/:id")
+  .get(booksController.findMany)
+  .put(booksController.update)
+  .delete(booksController.remove);
+
+module.exports = router;
